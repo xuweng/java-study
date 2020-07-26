e601719431178a5b88e989d45d649c4eb701062a
xuweng <2028403305@qq.com>
2020-07-26 13:04:16 +0800

    java学习项目构建

---
+++ java-concurrent/src/test/java/org/java/AppTest.java
@@ -0,0 +1,14 @@
+package org.java;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
