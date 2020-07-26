1ea26d56e0a4dfe07ad1fefcd81398ddf3f0e178
xuweng <2028403305@qq.com>
2020-07-26 22:01:31 +0800

    手写tomcat

--- java-minitomcat/src/main/java/org/java/tomcat/MyServlet.java
+++ java-minitomcat/src/main/java/org/java/tomcat/MyServlet.java
@@ -13,9 +13,9 @@ public abstract class MyServlet {
     public abstract void doPost(MyRequest myRequest, MyResponse myResponse);
 
     public void service(MyRequest myRequest, MyResponse myResponse) {
-        if (myRequest.getMethod().equalsIgnoreCase("POST")) {
+        if ("POST".equalsIgnoreCase(myRequest.getMethod())) {
             doPost(myRequest, myResponse);
-        } else if (myRequest.getMethod().equalsIgnoreCase("GET")) {
+        } else if ("GET".equalsIgnoreCase(myRequest.getMethod())) {
             doGet(myRequest, myResponse);
         }
     }
