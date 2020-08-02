2a5c0dd189bce5a7797471ade240a65dc2f09e4e
xuweng <2028403305@qq.com>
2020-08-02 23:01:00 +0800

    生产者和消费者线程
    Signed-off-by: xuweng <2028403305@qq.com>

--- concurrent-program-beauty/src/main/java/org/java/one/three/Main.java
+++ concurrent-program-beauty/src/main/java/org/java/one/three/Main.java
@@ -46,10 +46,10 @@ public class Main {
     public static void main(String[] args) {
         new Thread(() -> {
             new Example.ProducerThread().produce(1);
-        });
+        }).start();
 
         new Thread(() -> {
             new Example.ConsumerThread().consumption();
-        });
+        }).start();
     }
 }
