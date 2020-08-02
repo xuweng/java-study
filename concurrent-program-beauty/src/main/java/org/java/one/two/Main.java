44a3d7ea014506c659bdce1dee5a7026bab3d35c
xuweng <2028403305@qq.com>
2020-08-02 18:35:49 +0800

    看书+跑代码。
    Signed-off-by: xuweng <2028403305@qq.com>

--- concurrent-program-beauty/src/main/java/org/java/one/two/Main.java
+++ concurrent-program-beauty/src/main/java/org/java/one/two/Main.java
@@ -1,5 +1,10 @@
 package org.java.one.two;
 
+/**
+ * 看书的同时跑一下书本里面的代码。仅仅看是没用的。
+ * <p>
+ * 看书+跑代码。
+ */
 public class Main {
     public static class RunnableTask implements Runnable {
         @Override
