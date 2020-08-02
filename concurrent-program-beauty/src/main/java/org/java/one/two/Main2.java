cbc58d437c1e21dfa337a50e4a30daca172e7794
xuweng <2028403305@qq.com>
2020-08-02 22:14:33 +0800

    第一次使用FutureTask
    Signed-off-by: xuweng <2028403305@qq.com>

--- concurrent-program-beauty/src/main/java/org/java/one/two/Main2.java
+++ concurrent-program-beauty/src/main/java/org/java/one/two/Main2.java
@@ -1,6 +1,8 @@
 package org.java.one.two;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
 /**
  * https://blog.csdn.net/kealina/article/details/78376795
@@ -8,6 +10,7 @@ import java.util.concurrent.Callable;
  * IDEA快捷键设置复制上一行
  */
 public class Main2 {
+    //创建任务类，类似Runnable
     public static class CallerTask implements Callable<String> {
         @Override
         public String call() throws Exception {
@@ -15,7 +18,13 @@ public class Main2 {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        FutureTask<String> futureTask = new FutureTask<>(new CallerTask());
+        //启动线程
+        new Thread(futureTask).start();
+        //等待任务执行完毕，并返回结果
+        String result = futureTask.get();
 
+        System.out.println(result);
     }
 }
